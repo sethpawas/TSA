d0820dab133cbeffc061b94230e11209d510f5b9
sailakshmian <94903633+sailakshmian@users.noreply.github.com>
2021-11-23 21:47:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ import time
 @st.cache								
 def load_model(input_data):
 
-	pickle_in = open(r'https://github.com/sailakshmian/TSA/blob/main/HF_py_1011.pkl', 'rb') 
+	pickle_in = open(r'HF_py_1011.pkl', 'rb') 
 	xgb = pickle.load(pickle_in)
 	pickle_in.close()
 
@@ -59,7 +59,7 @@ def load_model(input_data):
 	return pred
 
 def main():
-	TSA_data = pd.read_csv(r"https://github.com/sailakshmian/TSA/blob/main/TSA_TS7.csv")
+	TSA_data = pd.read_csv(r"TSA_TS7.csv")
 
 	list_24 = ['Release_Dose_Calc_mg_m' ,'HEADBOX_PRES_psig' ,'Steam_Box_Pressure_PSIG' ,'PM15_Winder_Speed_FPM' ,'Refiner_1_HPD_T' ,'UHLEBOX1_VAC_Inhg'
 		,'Hi_Bright_PM15_Ratio' ,'EosYMoiPv_pct' ,'Coating_Dose_Calc_mg_m' ,'Refiner_3_HP_Control' ,'Metso_Rush_Drag_to_DV' ,'EUC_PM15_Ratio' 
